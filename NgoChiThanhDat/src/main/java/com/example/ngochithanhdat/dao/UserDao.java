863e5b5d7e94ac4f5dd106cb3efdb465adcafb93
ngochithanhdat02 <ngochithanhdat02@gmail.com>
2022-09-30 20:32:18 +0700

    up

---
+++ NgoChiThanhDat/src/main/java/com/example/ngochithanhdat/dao/UserDao.java
@@ -0,0 +1,27 @@
+package com.example.ngochithanhdat.dao;
+import com.example.ngochithanhdat.model.User;
+import com.example.ngochithanhdat.utils.ConnectionUtils;
+
+import java.sql.*;
+
+public class UserDao {
+    Connection connection = ConnectionUtils.getConnection();
+
+    public User getUser(String pName, String password) {
+        User user = null;
+        String sql = "select * from users where username = '"+pName +"' and password = " +password;
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()){
+                String name = rs.getString("username");
+                String pass = rs.getString("password");
+                user = new User(name, pass);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return user;
+    }
+
+}
